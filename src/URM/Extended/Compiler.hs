cf272cb815eced838b9614f3769ac28489000338
ggzor <30713864+ggzor@users.noreply.github.com>
2020-02-14 19:12:35 +0000

    Adding support for bounded products and minimizations

--- src/URM/Extended/Compiler.hs
+++ src/URM/Extended/Compiler.hs
@@ -25,15 +25,30 @@ data CompilerState = CompilerState { _loadedPrograms :: !Env }
 makeLenses ''CompilerState
 
 builtIns = Prelude.foldl (\env (name, body) -> M.insert name (body env) env) M.empty 
-            [("__builtin_successor", successor), ("__builtin_sum", sum)]
+            [ ("__builtin_successor", successor)
+            , ("__builtin_signum", signum)
+            , ("__builtin_sum", sum)
+            , ("__builtin_product", product)]
   where successor = const . V.singleton $ Successor 1
+        signum = const . V.fromList $
+                   [ Jump 1 2 4
+                   , Successor 2
+                   , Transfer 2 1 ]
         sum = const . V.fromList $ 
                 [ Jump 2 3 5
                 , Successor 1
                 , Successor 3
                 , Jump 1 1 1]
+        product env = fromMaybe undefined $ fromRecursiveDeclaration env
+                        ("__builtin_product", ["x"], "y"
+                            , Constant 0
+                            , Call "__builtin_sum" [Call "__builtin_product" [Name "x", Name "y"], Name "x"])
 
-removeBuiltIns = M.delete "__builtin_successor" . M.delete "__builtin_sum"
+removeBuiltIns = 
+    M.delete "__builtin_successor" 
+  . M.delete "__builtin_signum"
+  . M.delete "__builtin_sum" 
+  . M.delete "__builtin_product"
 
 compileEURM :: [EURM] -> Either EURMCompilationError Env
 compileEURM programs = Right $ removeBuiltIns (evalState (processPrograms programs) $ CompilerState builtIns)
@@ -46,23 +61,59 @@ compileEURM programs = Right $ removeBuiltIns (evalState (processPrograms progra
                                 , x ^? _CompositeDeclaration  >>= fromCompositeDeclaration env
                                 , x ^? _AliasDeclaration._2   >>= flip M.lookup env
                                 , x ^? _RecursiveDeclaration  >>= fromRecursiveDeclaration env
-                                , x ^? _BoundedSumDeclaration >>= fromBoundedSumDeclaration env]
+                                , x ^? _BoundedSumDeclaration >>= fromBoundedSumDeclaration env
+                                , x ^? _BoundedProductDeclaration >>= fromBoundedProductDeclaration env
+                                , x ^? _BoundedMinimizationDeclaration >>= fromBoundedMinimizationDeclaration env]
              newProgram & maybe (pure ()) (\instructions -> 
                loadedPrograms %= M.insert newProgramName (standarizeURM instructions))
              processPrograms xs
 
+type BoundedMinimizationDeclarationFields = (String, [Variable], Variable, Expression, Expression)
+fromBoundedMinimizationDeclaration :: Env -> BoundedMinimizationDeclarationFields -> Maybe Instructions
+fromBoundedMinimizationDeclaration env (name, parameters, index, top, body) =
+  let innerIndexName = "__innerIndex"
+      innerBody = Call "__builtin_signum" [replaceName index innerIndexName body]
+      outerIndexName = "__outerIndex"
+      productProgramName = "__innerProduct"
+      productProgram = fromBoundedProductDeclaration env 
+                         (productProgramName, parameters ++ [outerIndexName], innerIndexName
+                         , Call "__builtin_successor" [Name outerIndexName]
+                         , innerBody)
+      sumProgramName = "__outerSum"
+      modifiedEnv = (\p -> env & at productProgramName ?~ p) <$> productProgram
+ in flip fromBoundedSumDeclaration
+      ( sumProgramName
+      , parameters, outerIndexName, top
+      , Call productProgramName ((Name <$> parameters) ++ [Name outerIndexName])) =<< modifiedEnv
+
+replaceName :: String -> String -> Expression -> Expression
+replaceName original new (Name actual) | original == actual = Name new
+replaceName original new (Call callName params) = Call callName (replaceName original new <$> params)
+replaceName _ _ other = other
+
+type BoundedProductDeclarationFields = (String, [Variable], Variable, Expression, Expression)
+fromBoundedProductDeclaration :: Env -> BoundedProductDeclarationFields -> Maybe Instructions
+fromBoundedProductDeclaration env (name, parameters, index, top, body) = 
+  let namedParams = Name <$> parameters
+      auxiliarProgramName = "__recursiveBoundedProduct"
+      auxiliarRecursiveStep = Call "__builtin_product" [Call auxiliarProgramName (namedParams ++ [Name index]), body] 
+      auxiliarProgram = fromRecursiveDeclaration env 
+                          (auxiliarProgramName, parameters, index, Constant 1, auxiliarRecursiveStep)
+      modifiedEnv = (\p -> env & at auxiliarProgramName ?~ p) <$> auxiliarProgram
+      finalExpression = Call auxiliarProgramName (namedParams ++ [top])
+  in flip fromCompositeDeclaration (name, parameters, finalExpression) =<< modifiedEnv
+
 type BoundedSumDeclarationFields = (String, [Variable], Variable, Expression, Expression)
 fromBoundedSumDeclaration :: Env -> BoundedSumDeclarationFields -> Maybe Instructions 
 fromBoundedSumDeclaration env (name, parameters, index, top, body) =
-  let indexedParams = M.fromList $ zip parameters [1..]
-      namedParams = Name <$> parameters
+  let namedParams = Name <$> parameters
       auxiliarProgramName = "__recursiveBoundedSum"
       auxiliarRecursiveStep = Call "__builtin_sum" [Call auxiliarProgramName (namedParams ++ [Name index]), body]
       auxiliarProgram = fromRecursiveDeclaration env
                           (auxiliarProgramName, parameters, index, Constant 0, auxiliarRecursiveStep)
       modifiedEnv = (\p -> env & at auxiliarProgramName ?~ p) <$> auxiliarProgram
       finalExpression = Call auxiliarProgramName (namedParams ++ [top])                      
-  in join $ expressionToProgram indexedParams <$> modifiedEnv <*> pure finalExpression
+  in flip fromCompositeDeclaration (name, parameters, finalExpression) =<< modifiedEnv
 
 type CompositeDeclarationFields = (String, [Variable], Expression)
 fromCompositeDeclaration :: Env -> CompositeDeclarationFields -> Maybe Instructions
