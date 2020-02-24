23620a37ec53f145dcf38da750bd6d672204e063
ggzor <30713864+ggzor@users.noreply.github.com>
2020-02-24 06:17:00 +0000

    Adding explicit forall annotation to Input utils

--- src/Polysemy/Input/Utils.hs
+++ src/Polysemy/Input/Utils.hs
@@ -7,5 +7,5 @@ import Polysemy.State
 inputToState :: Member (State s) r => Sem (Input s ': r) a -> Sem r a
 inputToState = runInputSem get
 
-mapInput :: Member (Input i2) r => (i2 -> i1) -> Sem (Input i1 ': r) a -> Sem r a
+mapInput :: forall i1 i2 r a. Member (Input i2) r => (i2 -> i1) -> Sem (Input i1 ': r) a -> Sem r a
 mapInput f = runInputSem $ f <$> input
