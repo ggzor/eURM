f0963c4c292e12958df22909e75a703e66c977af
ggzor <30713864+ggzor@users.noreply.github.com>
2020-02-24 08:46:05 +0000

    Adding forall to input

--- src/Polysemy/Input/Utils.hs
+++ src/Polysemy/Input/Utils.hs
@@ -4,7 +4,7 @@ import Polysemy
 import Polysemy.Input
 import Polysemy.State
 
-inputToState :: Member (State s) r => Sem (Input s ': r) a -> Sem r a
+inputToState :: forall s r a. Member (State s) r => Sem (Input s ': r) a -> Sem r a
 inputToState = runInputSem get
 
 mapInput :: forall i1 i2 r a. Member (Input i2) r => (i2 -> i1) -> Sem (Input i1 ': r) a -> Sem r a
